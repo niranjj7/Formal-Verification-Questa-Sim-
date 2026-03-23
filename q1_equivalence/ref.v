26cc64f4a2b583398c0e13bdbb44ab0e14174f83
K S Niranjan <niranjanks838@gmail.com>
2026-03-24 01:07:37 +0530

    Add reference design for AND gate circuit

--- q1_equivalence/ref.v
+++ q1_equivalence/ref.v
@@ -1,1 +1,6 @@
-
+// Circuit A - Reference Design
+// Computes y = a AND b using direct AND gate
+module ref(input clk, input a, input b, output reg y);
+    always @(posedge clk)
+        y <= a & b;
+endmodule
